d70eb91ed71830fb7d7430c5b122761309e1a3fd
Kaique Melhado <kaique16souza@hotmail.com>
2025-03-27 16:52:37 -0300

    feat(configuration): centralização de injeção de dependência, logging com Serilog e integração com FluentValidation e MediatR

---
+++ src/FinTrack.Configuration/Logging/SerilogConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace FinTrack.Configuration.Logging;
+
+/// <summary>
+/// Classe responsável por configurar o Serilog.
+/// </summary>
+public static class SerilogConfiguration
+{
+    /// <summary>
+    /// Adiciona a configuração padrão do Serilog ao host builder.
+    /// </summary>
+    /// <param name="builder">Host builder da aplicação.</param>
+    /// <returns>Instância configurada do host builder.</returns>
+    public static IHostBuilder UseSerilogConfiguration(this IHostBuilder builder)
+    {
+        return builder.UseSerilog((context, services, loggerConfig) =>
+        {
+            var env = context.HostingEnvironment;
+            var configuration = context.Configuration;
+
+            loggerConfig
+                .ReadFrom.Configuration(configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Environment", env.EnvironmentName)
+                .WriteTo.Console()
+                .WriteTo.File(
+                    path: "logs/fintrack-log.txt",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 7,
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+                );
+        });
+    }
+}
---
+++ src/FinTrack.Configuration/Settings/AlphaVantageSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace FinTrack.Configuration.Settings;
+
+/// <summary>
+/// Representa as configurações para o cliente Alpha Vantage.
+/// </summary>
+public class AlphaVantageSettings
+{
+    /// <summary>
+    /// Chave de API utilizada para autenticação nas requisições ao Alpha Vantage.
+    /// </summary>
+    public string ApiKey { get; set; } = default!;
+
+    /// <summary>
+    /// URL base para as requisições à API do Alpha Vantage.
+    /// </summary>
+    public string BaseUrl { get; set; } = default!;
+}
